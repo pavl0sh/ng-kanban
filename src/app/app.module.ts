25c035f669dc7d74f90f2ccfd71c6b88ebcf1cdd
pavl0sh <pavlo.shvoruk@gmail.com>
2020-01-18 16:52:55 +0200

    google signin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { HomePageComponent } from './home-page/home-page.component';
 
+import { environment } from '../environments/environment';
+
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [
@@ -17,7 +22,10 @@ import { HomePageComponent } from './home-page/home-page.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-  
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCWhg9NH74MDxZ6SGNtyz-7ngrq25TretE",
+    authDomain: "ng-kanban-1637a.firebaseapp.com",
+    databaseURL: "https://ng-kanban-1637a.firebaseio.com",
+    projectId: "ng-kanban-1637a",
+    storageBucket: "ng-kanban-1637a.appspot.com",
+    messagingSenderId: "932932829610",
+    appId: "1:932932829610:web:02f9f22377c6fdf6132bd3",
+    measurementId: "G-0R9LBSLWK5"
+  }
 };
 
 /*
